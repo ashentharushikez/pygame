37899511b10c28ceeb721bf5314fbfb752f1a9ea
ashentharushikez <145181570+ashentharushikez@users.noreply.github.com>
2024-11-22 18:36:39 +0530

    Add files via upload

--- car_game.py
+++ car_game.py
@@ -41,7 +41,48 @@ lanes = [left_lane, center_lane, right_lane]
 # for animating movement of the lane markers
 lane_marker_move_y = 0
 
+class Vehicle(pygame.sprite.Sprite):
 
+    def __init__(self, image, x, y):
+        pygame.sprite.Sprite.__init__(self)
+
+        # scale the image down so it fits in the lane
+        image_scale = 45 / image.get_rect().width
+        new_width = image.get_rect().width * image_scale
+        new_height = image.get_rect().height * image_scale
+        self.image = pygame.transform.scale(image, (new_width, new_height))
+
+        self.rect = self.image.get_rect()
+        self.rect.center = [x, y]
+
+class PlayerVehicle(Vehicle):
+
+    def __init__(self, x, y):
+        image = pygame.image.load('images/car.png')
+        super().__init__(image, x, y)
+
+# Player's starting coordinates
+player_x = 250
+player_y = 400
+
+# Create the player's car
+player_group = pygame.sprite.Group()
+player = PlayerVehicle(player_x, player_y)
+player_group.add(player)
+
+# load the other vehicle images
+image_filenames = ['Black_viper.png', 'truck.png', 'taxi.png', 'Audi.png', 'Mini_van.png']
+vehicle_images = []
+for image_filename in image_filenames:
+    image = pygame.image.load('images/' + image_filename)
+    vehicle_images.append(image)
+
+# sprite group for vehicles
+vehicle_group = pygame.sprite.Group()
+
+# Load the crash image
+crash = pygame.image.load('images/crash.png')
+crash_rect = crash.get_rect()
 
 # game Loop
 clock = pygame.time.Clock()
@@ -54,6 +95,31 @@ while running:
     for event in pygame.event.get():
         if event.type == QUIT:
             running = False
+
+        # move the player's car using the left/right arrow keys   
+        if event.type == KEYDOWN:
+
+            if event.key == K_LEFT and player.rect.center[0] > left_lane:
+                 player.rect.x -= 100
+            elif event.key == K_RIGHT and player.rect.center[0] < right_lane:
+                 player.rect.x += 100   
+
+            # chech if there's a side swipe collision after changing lanes
+            for vehicle in vehicle_group:
+                if pygame.sprite.collide_rect(player, vehicle):
+
+                    gameover = True
+
+                    # place the player's car next to another vehicle
+                    # and determine where to position the crash image
+                    if event.key == K_LEFT:
+                       player.rect.left = vehicle.rect.right
+                       crash_rect.center = [player.rect.left, (player.rect.center[1] + vehicle.rect.center[1]) / 2]
+                    elif event.key == K_RIGHT:
+                       player.rect.right = vehicle.rect.left
+                       crash_rect.center = [player.rect.right, (player.rect.center[1] + vehicle.rect.center[1]) / 2]
+
+
 
     # draw the grass
     screen.fill(green)
@@ -73,8 +139,102 @@ while running:
        pygame.draw.rect(screen, white, (left_lane + 45, y + lane_marker_move_y, marker_width, marker_height))
        pygame.draw.rect(screen, white, (center_lane + 45, y + lane_marker_move_y, marker_width, marker_height))
 
+    # draw the player's car
+    player_group.draw(screen)
+
+    # add up to two vehicles
+    if len(vehicle_group) < 2:
+
+       # ensure there's enough gap between vehicles
+       add_vehicle = True
+       for vehicle in vehicle_group:
+          if vehicle.rect.top < vehicle.rect.height * 1.5:
+            add_vehicle = False
+
+       if add_vehicle:
+
+          # select a random lane
+          lane = random.choice(lanes)
+
+          # select a random vehicle image
+          lane = random.choice(lanes)
+
+          # select a random vehicle image
+          image = random.choice(vehicle_images)
+          vehicle = Vehicle(image, lane, height / -2)
+          vehicle_group.add(vehicle)
+
+    # make the vehicles move
+    for vehicle in vehicle_group:
+        vehicle.rect.y += speed
+
+        # remove the vehicle once it goes off screen
+        if vehicle.rect.top >= height:
+            vehicle.kill()
+
+            # add to score
+            score += 1
+
+            # speed up the game after passing 5 vehicles
+            if score > 0 and score % 5 == 0:
+                speed += 1
+       
+    # draw the vehicles
+    vehicle_group.draw(screen)
+
+    # display the score
+    font = pygame.font.Font(pygame.font.get_default_font(), 16)
+    text = font.render('Score: ' + str(score), True, white)
+    text_rect = text.get_rect()
+    text_rect.center = (50, 450)
+    screen.blit(text, text_rect)
+
+    # check if there's a head-on collision
+    if pygame.sprite.spritecollide(player, vehicle_group, True):
+       gameover = True
+       crash_rect.center = [player.rect.center[0], player.rect.top]
+
+    # display game over
+    if gameover:
+       screen.blit(crash, crash_rect)
+
+       pygame.draw.rect(screen, red, (0, 50, width, 100))
+
+       font = pygame.font.Font(pygame.font.get_default_font(), 16)
+       text = font.render('Game over. Play again? (Enter Y or N)', True, white)
+       text_rect = text.get_rect()
+       text_rect.center = (width / 2, 100)
+       screen.blit(text, text_rect)
+
 
     pygame.display.update()
+
+    pygame.display.update()
+
+    # check if player wants to play again
+    while gameover:
+
+        clock.tick(fps)
+
+        for event in pygame.event.get():
+
+            if event.type == QUIT:
+                gameover = False
+                running = False
+
+            # get the player's input (y or n)
+            if event.type ==  KEYDOWN:
+                if event.key == K_y:
+                    # reset the game
+                    gameover = False
+                    speed = 2
+                    score = 0
+                    vehicle_group.empty()  
+                    player.rect.center = [player_x, player_y]
+                elif event.key == K_n:
+                    # exit the Loops
+                    gameover = False
+                    running = False    
     
 
 pygame.quit()    
